8ca65768f529cabb5b591e0ee996480000237666
Jakku Night <J97k107u45k117n@gmail.com>
2022-09-02 01:32:36 +0000

    NativeBase support added. Must migrate to newer versions when released.

--- frontend/client/App.js
+++ frontend/client/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {} from "react";
+import { useWindowDimensions } from "react-native";
+import { NativeBaseProvider, Center, Heading, Text } from "native-base";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+	const screenSize = useWindowDimensions();
+	return (
+		<NativeBaseProvider>
+			<Center h={screenSize.height} w={screenSize.width} size={"100%"} bgColor={"black"} >
+				<Heading color={"white"} >
+					{"Hello Doumo App!!!"}
+				</Heading>
+				<Text color={"#AAAAAA"} >
+					{"Hello Doumo!"}
+				</Text>
+			</Center>
+		</NativeBaseProvider>
+	);
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
